656bd6bbdf8729bd944638688049932cc4dbc44f
chu-young <ttheko101@gmail.com>
2021-04-03 11:21:03 +0200

    added ajax request and base for inner html

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-console.log('ready')
+$('#searchUser').on('keyup', function(e){
+    let username = e.target.value;
+   
+    //Make request to Github using AJAX
+    $.ajax({
+        url:'https://api.github.com/users/'+username,
+        data:{
+            client_id:'d88a76b43ae87295350e',
+            client_secret:'23d175f5feb94b769284c572668ae0044dc58805'
+
+        }
+    }).done(function(user){
+$('#profile').html(`
+${user.name}
+`)
+    })
+})
 });
