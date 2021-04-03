95d8f025e5217fc1ae493f63a85e981a8c40a59c
chu-young <ttheko101@gmail.com>
2021-04-03 15:49:36 +0200

    added basic card structure

--- app.js
+++ app.js
@@ -12,7 +12,20 @@ $('#searchUser').on('keyup', function(e){
         }
     }).done(function(user){
 $('#profile').html(`
-${user.name}
+
+<div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" style='width:100%' src="${user.avatar_url}">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">${user.name}<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">${user.name}<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
 `)
     })
 })
